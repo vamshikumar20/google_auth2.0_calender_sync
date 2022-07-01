ad8bf37e53e2ec88de2da6f5321df9d09ca2c04e
Prashant <verma.prashant15@gmail.com>
2022-07-01 16:19:42 +0530

    fix views

--- rest/views.py
+++ rest/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 import json
 import os.path
+import pickle
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -9,7 +10,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-# If modifying these scopes, delete the file token.json.
+# If modifying these scopes, delete the file token.pkl.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
@@ -17,11 +18,12 @@ def google_oauth():
     """Shows basic usage of the Google Calendar API and get the user token
     """
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
+    # The file token.pkl stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists('token.pkl'):
+        credentials_pickle = pickle.load(open("token.pkl", "rb"))
+        creds = Credentials.from_authorized_user_file(credentials_pickle, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -31,40 +33,40 @@ def google_oauth():
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+        pickle.dump(creds, open("token.pkl", "wb"))
     return creds
 
-
+# get token credentials
 @api_view(['GET'])
 def GoogleCalendarInitView(request):
-
-    if os.path.exists('token.json') == True:
-        with open('token.json', 'r') as token:
-            token_private = json.load(token)
-        return Response({"token": token_private})
+    if os.path.exists('token.pkl') == True:
+        credentials_pickle = pickle.load(open("token.pkl", "rb"))
+        return Response({credentials_pickle.to_json()})
     else:
         creds = google_oauth()
-        with open('token.json', 'r') as token:
-            token_private = json.load(token)
-        return Response({"token": token_private})
+        credentials_pickle = pickle.load(open("token.pkl", "rb"))
+        return Response({credentials_pickle.to_json()})
 
-
+# get user calendar events
 @api_view(['GET'])
 def GoogleCalendarRedirectView(request):
 
-    credentials = google_oauth()
-    service = build("calendar", "v3", credentials=credentials)
-    # Call the Sheets API
-    calendar_list = service.calendarList().list().execute()
-    calendar_id = calendar_list['items'][0]['id']
-    events  = service.events().list(calendarId=calendar_id).execute()
+    if os.path.exists('token.pkl') == True:
+        credentials_pickle = pickle.load(open("token.pkl", "rb"))
+        service = build("calendar", "v3", credentials=credentials_pickle)
+        # Call the Sheets API
+        calendar_list = service.calendarList().list().execute()
+        calendar_id = calendar_list['items'][0]['id']
+        events  = service.events().list(calendarId=calendar_id).execute()
 
-    events_list_append = []
-    if not events ['items']:
-        print('No data found.')
-        return Response({"Error": "No data found."})
+        events_list_append = []
+        if not events['items']:
+            print('No data found.')
+            return Response({"message": "No data found."})
+        else:
+            for events_list in events['items']:
+                events_list_append.append(events_list)
+        return Response({"events": events_list_append})
     else:
-        for events_list in events['items']:
-            events_list_append.append(events_list)
-    return Response({"events": events_list_append})
+        return Response({"create token": token})
+
